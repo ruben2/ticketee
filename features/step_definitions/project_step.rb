661a95f9f783405482258df8da398d9466f293b5
ruben2 <rsp_savo@yahoo.es>
2011-10-04 12:48:12 +0200

    Added title functionality for show page

--- features/step_definitions/project_step.rb
+++ features/step_definitions/project_step.rb
@@ -21,3 +21,30 @@ Then /^(?:|I )should see "([^"]*)"$/ do |text|
     assert page.has_content?(text)
   end
 end
+
+Then /^(?:|I )should see \/([^\/]*)\/$/ do |regexp|
+  regexp = Regexp.new(regexp)
+
+  if page.respond_to? :should
+    page.should have_xpath('//*', :text => regexp)
+  else
+    assert page.has_xpath?('//*', :text => regexp)
+  end
+end
+
+Then /^(?:|I )should not see "([^"]*)"$/ do |text|
+  if page.respond_to? :should
+    page.should have_no_content(text)
+  else
+    assert page.has_no_content?(text)
+  end
+end
+
+Then /^(?:|I )should be on (.+)$/ do |page_name|
+  current_path = URI.parse(current_url).path
+  if current_path.respond_to? :should
+    current_path.should == path_to(page_name)
+  else
+    assert_equal path_to(page_name), current_path
+  end
+end
--- features/support/path.rb
+++ features/support/path.rb
@@ -6,7 +6,6 @@ def path_to(page_name)
     when /the project page for "([^\"]*)"/
       project_path(Project.find_by_name!($1))
 
-
     else
       raise "Can't find mapping from \"#{page_name}\" to a path."
   end
