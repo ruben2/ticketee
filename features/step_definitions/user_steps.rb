190fa961fe503baaf87e6a85b2e3dbaff2998004
ruben2 <rsp_savo@yahoo.es>
2011-10-05 16:34:47 +0200

    Restrict access to project actions to admins only

--- features/step_definitions/user_steps.rb
+++ features/step_definitions/user_steps.rb
@@ -2,9 +2,11 @@ Given /^there are the following users:$/ do |table|
   table.hashes.each do |attributes|
     unconfirmed = attributes.delete("unconfirmed") == "true"
     @user = User.create!(attributes)
+    @user.update_attribute("admin", attributes["admin"] == "true")
     @user.confirm! unless unconfirmed
   end
 end
+
 
 Given /^I am signed in as them$/ do
   steps(%Q{
