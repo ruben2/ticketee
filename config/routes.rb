dd2ea47230093d368ab27573275b3732c948617e
ruben2 <rsp_savo@yahoo.es>
2011-10-07 12:22:52 +0200

    Assets are now strictly served through FilesController

--- config/routes.rb
+++ config/routes.rb
@@ -55,6 +55,8 @@ Ticketee::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
+  resources :files
+
   resources :projects do
     resources :tickets
   end
