d45273396d3730d214314691c739e5375ecaeb5b
ruben2 <rsp_savo@yahoo.es>
2011-10-13 09:09:50 +0200

    Add button so users can toggle watching on a ticket

--- app/controllers/tickets_controller.rb
+++ app/controllers/tickets_controller.rb
@@ -1,7 +1,7 @@
 class TicketsController < ApplicationController
 
   before_filter :find_project
-  before_filter :find_ticket, :only => [:show, :edit, :update, :destroy]
+  before_filter :find_ticket, :only => [:show, :edit, :update, :destroy, :watch]
   before_filter :authenticate_user!
   before_filter :authorize_create!, :only => [:new, :create]
   before_filter :authorize_update!, :only => [:edit, :update]
@@ -75,6 +75,17 @@ class TicketsController < ApplicationController
       flash[:alert] = "You cannot delete tickets from this project."
       redirect_to @project
     end
+  end
+
+  def watch
+    if @ticket.watchers.exists?(current_user)
+      @ticket.watchers -= [current_user]
+      flash[:notice] = "You are no longer watching this ticket."
+    else
+      @ticket.watchers << current_user
+      flash[:notice] = "You are now watching this ticket."
+    end
+    redirect_to project_ticket_path(@ticket.project, @ticket)
   end
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -62,6 +62,9 @@ Ticketee::Application.routes.draw do
       collection do
         get :search
       end
+      member do
+        post :watch
+      end
     end
   end
 
